3cbb6e1f9369bfb16957a37e0a4df34e86849d6e
Xavier BROQUERE <xavier.broquere@renault.com>
2019-07-20 15:30:51 +0200

    add moonwalk model from gazebo to test dynamic update of color and different cases
    Signed-off-by: Xavier BROQUERE <xavier.broquere@renault.com>

--- marker_mesh_sample.py
+++ marker_mesh_sample.py
@@ -2,12 +2,18 @@
 
 import rospy
 from visualization_msgs.msg import Marker
+from visualization_msgs.msg import MarkerArray
 
 rospy.init_node("marker_mesh_sample")
 
 pub = rospy.Publisher('marker', Marker)
 pub2 = rospy.Publisher('marker2', Marker)
+moonwalk_markers_pub = rospy.Publisher('moonwalk_markers', MarkerArray)
+
 r = rospy.Rate(10)
+
+green = 0
+
 while not rospy.is_shutdown():
     mesh_marker = Marker()
     
@@ -28,4 +34,121 @@ while not rospy.is_shutdown():
     mesh_marker.mesh_resource = "package://rviz_collada_marker/base.dae"
     mesh_marker.pose.position.y = 2.0
     pub2.publish(mesh_marker)
+
+    moonwalk_markers = MarkerArray()
+
+    moonwalk1 = Marker()
+    moonwalk1.header.frame_id = "/nil_link"
+    moonwalk1.header.stamp = rospy.Time.now()
+    moonwalk1.ns = "moonwalk"
+    moonwalk1.id = 0
+    moonwalk1.type = Marker.MESH_RESOURCE
+    moonwalk1.mesh_resource = "package://rviz_collada_marker/moonwalk.dae"
+    moonwalk1.mesh_use_embedded_materials=False
+
+    moonwalk1.pose.position.x = 0
+    moonwalk1.pose.position.y = 0
+    moonwalk1.pose.position.z = 0
+    moonwalk1.pose.orientation.x = 0.0
+    moonwalk1.pose.orientation.y = 0.0
+    moonwalk1.pose.orientation.z = 0.0
+    moonwalk1.pose.orientation.w = 1.0
+    moonwalk1.scale.x = 1.0
+    moonwalk1.scale.y = 1.0
+    moonwalk1.scale.z = 1.0
+    moonwalk1.color.r = 0
+    moonwalk1.color.g = green
+    moonwalk1.color.b = 0
+    moonwalk1.color.a = 0.2
+ 
+    moonwalk_markers.markers.append(moonwalk1)
+
+    moonwalk2 = Marker()
+    moonwalk2.header.frame_id = "/nil_link"
+    moonwalk2.header.stamp = rospy.Time.now()
+    moonwalk2.ns = "moonwalk"
+    moonwalk2.id = 1
+    moonwalk2.type = Marker.MESH_RESOURCE
+    moonwalk2.mesh_resource = "package://rviz_collada_marker/moonwalk.dae"
+    moonwalk2.mesh_use_embedded_materials=True
+ 
+    moonwalk2.pose.position.x = 2
+    moonwalk2.pose.position.y = 0
+    moonwalk2.pose.position.z = 0
+    moonwalk2.pose.orientation.x = 0.0
+    moonwalk2.pose.orientation.y = 0.0
+    moonwalk2.pose.orientation.z = 0.0
+    moonwalk2.pose.orientation.w = 1.0
+    moonwalk2.scale.x = 1.0
+    moonwalk2.scale.y = 1.0
+    moonwalk2.scale.z = 1.0
+    moonwalk2.color.r = 0
+    moonwalk2.color.g = 1
+    moonwalk2.color.b = 0
+    moonwalk2.color.a = 1
+   
+    moonwalk_markers.markers.append(moonwalk2)
+
+
+    moonwalk3 = Marker()
+    moonwalk3.header.frame_id = "/nil_link"
+    moonwalk3.header.stamp = rospy.Time.now()
+    moonwalk3.ns = "moonwalk"
+    moonwalk3.id = 2
+    moonwalk3.type = Marker.MESH_RESOURCE
+    moonwalk3.mesh_resource = "package://rviz_collada_marker/moonwalk.dae"
+    moonwalk3.mesh_use_embedded_materials=True
+    moonwalk3.pose.position.x = 4
+    moonwalk3.pose.position.y = 0
+    moonwalk3.pose.position.z = 0
+    moonwalk3.pose.orientation.x = 0.0
+    moonwalk3.pose.orientation.y = 0.0
+    moonwalk3.pose.orientation.z = 0.0
+    moonwalk3.pose.orientation.w = 1.0
+    moonwalk3.scale.x = 1.0
+    moonwalk3.scale.y = 1.0
+    moonwalk3.scale.z = 1.0
+    moonwalk3.color.r = 0
+    moonwalk3.color.g = green
+    moonwalk3.color.b = 0
+    moonwalk3.color.a = 0.3
+
+    moonwalk_markers.markers.append(moonwalk3)
+
+    moonwalk4 = Marker()
+    moonwalk4.header.frame_id = "/nil_link"
+    moonwalk4.header.stamp = rospy.Time.now()
+    moonwalk4.ns = "moonwalk"
+    moonwalk4.id = 3
+    moonwalk4.type = Marker.MESH_RESOURCE
+    moonwalk4.mesh_resource = "package://rviz_collada_marker/moonwalk.dae"
+    moonwalk4.mesh_use_embedded_materials=True
+
+    moonwalk4.pose.position.x = 6
+    moonwalk4.pose.position.y = 0
+    moonwalk4.pose.position.z = 0
+    moonwalk4.pose.orientation.x = 0.0
+    moonwalk4.pose.orientation.y = 0.0
+    moonwalk4.pose.orientation.z = 0.0
+    moonwalk4.pose.orientation.w = 1.0
+    moonwalk4.scale.x = 1.0
+    moonwalk4.scale.y = 1.0
+    moonwalk4.scale.z = 1.0
+    moonwalk4.color.r = 0
+    moonwalk4.color.g = green
+    moonwalk4.color.b = 0
+    moonwalk4.color.a = 0
+
+    moonwalk_markers.markers.append(moonwalk4)
+
+
+    moonwalk_markers_pub.publish(moonwalk_markers)
+
+    green += 0.1
+
+    if green > 1:
+	green = 0
+   
     r.sleep()
+    
+
